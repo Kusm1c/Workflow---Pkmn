d7beb13c57035023de25c4be11c834d874a237fd
Damien <damien.harle@gmail.com>
2023-11-20 12:21:39 +0100

    SO move

--- Pokemon/Assets/Scripts/Pokemon/LearnableMoveSO.cs
+++ Pokemon/Assets/Scripts/Pokemon/LearnableMoveSO.cs
@@ -1,4 +1,7 @@
+using UnityEngine;
+
 public class LearnableMoveSO
 {
-        
+    [field: SerializeField] public int Level { get; private set; }
+    [field: SerializeField] public MoveSO Move { get; private set; }
 }
