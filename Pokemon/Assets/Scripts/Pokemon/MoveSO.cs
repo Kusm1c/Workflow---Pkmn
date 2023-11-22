3f46dc2481464dde33ad5a5fd98e9ae84b5535b6
Alexandre Mocoeur <alexandremocoeur@gmail.com>
2023-11-22 16:22:16 +0100

    feat : added buffs / debuffs / special attacks to fights calculations

--- Pokemon/Assets/Scripts/Pokemon/MoveSO.cs
+++ Pokemon/Assets/Scripts/Pokemon/MoveSO.cs
@@ -14,6 +14,7 @@ public class MoveSO : ScriptableObject
     [field: SerializeField] public int Priority { get; private set; }
     [field: SerializeField] public int CritRatio { get; private set; }
     [field: SerializeField] public StatusEffect StatusEffect { get; private set; }
+    [field: SerializeField] public Buff Buff { get; private set; }
     [field: SerializeField] public Target Target { get; private set; }
     
 }
