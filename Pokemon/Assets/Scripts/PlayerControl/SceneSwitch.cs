2f2b5a6a5454bc06ec2340e6ffb75069481b3fcb
Damien <damien.harle@gmail.com>
2023-11-23 12:01:36 +0100

    yep

--- Pokemon/Assets/Scripts/PlayerControl/SceneSwitch.cs
+++ Pokemon/Assets/Scripts/PlayerControl/SceneSwitch.cs
@@ -16,7 +16,7 @@ public class SceneSwitch : MonoBehaviour
     
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Return))
         {
             SwitchScene();
         }
